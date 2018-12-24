8a3d810816a443ae67d75df6287520e5a86825d9
Arvs Indarys <marc.majewski@live.fr>
2018-12-24 17:58:54 +0100

    Removed deprecated test
    Due to the new way of handling the paths, the missing method test is no longer of use.

--- tests/errors.test.js
+++ tests/errors.test.js
@@ -43,25 +43,3 @@ test('that missing route definition returns an error', () => {
     let routing = new Routing(options);
     expect(function(){routing.generate('persons.findByMyself');}).toThrow(new Error('No route defined with the following parameters: persons.findByMyself'));
 });
-
-test('that missing method returns an error', () => {
-
-    let options = {
-        routes: {
-            host: 'http://google.com',
-
-            persons: {
-                host: 'http://bing.com',
-                path: '/persons',
-
-                findAll: {
-                    host: 'ftp://yahoo.com/',
-                    path: '/all'
-                }
-            }
-        }
-    }
-
-    let routing = new Routing(options);
-    expect(function(){routing.generate('persons');}).toThrow(new Error('Both a resource and method must be specified'));
-});
